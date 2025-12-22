odoo/runbot

[IMP] runbot: allow to disable auto start of trigger depending on bundle type

runbot/models/repo.py
@@ -79,6 +79,9 @@ class Trigger(models.Model):
     ci_context = fields.Char("CI context", tracking=True)
     category_id = fields.Many2one('runbot.category', default=lambda self: self.env.ref('runbot.default_category', raise_if_not_found=False))
     version_domain = fields.Char(string="Version domain")
+    on_staging = fields.Boolean('Run on staging', default=True)
+    on_base = fields.Boolean('Run on base', default=True)
+    on_dev = fields.Boolean('Run on dev', deault=True)
     hide = fields.Boolean('Hide trigger on main page')
     manual = fields.Boolean('Only start trigger manually', default=False)
     restore_trigger_id = fields.Many2one('runbot.trigger', string='Restore Trigger ID for custom triggers', help="Mainly usefull to automatically define where to find a reference database when creating a custom trigger", tracking=True)

adhoc-cicd/odoo-runbot

runbot/models/repo.py
@@ -79,6 +79,9 @@ class Trigger(models.Model):
     ci_context = fields.Char("CI context", tracking=True)
     category_id = fields.Many2one('runbot.category', default=lambda self: self.env.ref('runbot.default_category', raise_if_not_found=False))
     version_domain = fields.Char(string="Version domain")
+    on_staging = fields.Boolean('Run on staging', default=True)
+    on_base = fields.Boolean('Run on base', default=True)
+    on_dev = fields.Boolean('Run on dev', default=True)
     hide = fields.Boolean('Hide trigger on main page')
     manual = fields.Boolean('Only start trigger manually', default=False)
     restore_trigger_id = fields.Many2one('runbot.trigger', string='Restore Trigger ID for custom triggers', help="Mainly usefull to automatically define where to find a reference database when creating a custom trigger", tracking=True)